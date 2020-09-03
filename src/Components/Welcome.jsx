2c02552dbaed5c866e26f12a6950e5e1105456c0
Monica <moniarbe233@gmail.com>
2020-09-02 21:38:53 -0500

    name

--- src/Components/Welcome.jsx
+++ src/Components/Welcome.jsx
@@ -7,7 +7,7 @@ import "./Styles/Welcome.css";
 
 export default class Welcome extends Component {
   render() {
-    const { ImagenMA, title2, text1 } = this.props;
+    const { ImagenMA, title1, text1 } = this.props;
     return (
       
       <div id="inicio">
@@ -16,7 +16,7 @@ export default class Welcome extends Component {
           <div className="row">
             <div  className="col-md-6 col-md-offset-6 align-self-center align-items-center justify-content-justify text-black ">
               
-              <h3 className="titulo1 text-white" >{title2}</h3>
+              <h3 className="titulo1 text-white" >{title1}</h3>
               <h1 className="texto1 text-white">{text1}</h1>
             </div>
             <div className="col-md-6 col-md-offset-6">
